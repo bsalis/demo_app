de452fde65ef66021e0006309acae4cf90bd18bf
Bowden Salis <bowden@handkrafted.com>
2014-07-13 17:25:15 +1000

    final tweaks

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   
   def index
+    @unicorns = `ps -ef | grep unicorn` || 'No unicorns it seems'
     @about = `aws ec2 describe-instances` || 'I seem to not be on AWS, hmmm'
   end
   
